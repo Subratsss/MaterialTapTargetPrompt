d14ee88f4af10b4f8c517be2d7ba32f578e2e6f0
Sam Wall <wallsamuelj@gmail.com>
2018-09-10 07:40:57 +0100

    trying to target view pager

--- sample/src/main/java/uk/co/samuelwall/materialtaptargetprompt/sample/SupportDialogFragmentExample.java
+++ sample/src/main/java/uk/co/samuelwall/materialtaptargetprompt/sample/SupportDialogFragmentExample.java
@@ -19,6 +19,10 @@ package uk.co.samuelwall.materialtaptargetprompt.sample;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,9 +34,11 @@ import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt;
  */
 public class SupportDialogFragmentExample extends DialogFragment
 {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+            Bundle savedInstanceState)
+    {
         View rootView = inflater.inflate(R.layout.fragment_dialog, container);
 
         getDialog().setTitle("DialogFragment");
@@ -45,24 +51,65 @@ public class SupportDialogFragmentExample extends DialogFragment
             }
         });
         rootView.findViewById(R.id.button_fab_prompt)
-            .setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View view)
+                .setOnClickListener(new View.OnClickListener()
                 {
-                    showFragmentFabPrompt();
-                }
-            });
+                    @Override
+                    public void onClick(View view)
+                    {
+                        showFragmentFabPrompt();
+                    }
+                });
+
+        ViewPager viewPager = rootView.findViewById(R.id.view_pager);
+        viewPager.setAdapter(new MyPagerAdapter(getChildFragmentManager(), this));
         return rootView;
     }
 
     public void showFragmentFabPrompt()
     {
-        new MaterialTapTargetPrompt.Builder(this)
+        new MaterialTapTargetPrompt.Builder((Fragment) this)
                 .setTarget(R.id.fab)
                 .setAnimationInterpolator(new FastOutSlowInInterpolator())
                 .setPrimaryText("Clipped to dialog bounds")
                 .setSecondaryText("The prompt does not draw outside the dialog")
                 .show();
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter
+    {
+        private static int ITEM_COUNT = 2;
+        private DialogFragment dialogFragment;
+
+        public MyPagerAdapter(FragmentManager fragmentManager, DialogFragment fragment)
+        {
+            super(fragmentManager);
+            dialogFragment = fragment;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return ITEM_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position)
+            {
+                case 0:
+                    return ViewPagerFragment.newInstance(dialogFragment);
+                case 1:
+                    return ViewPagerFragment.newInstance(dialogFragment);
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return "Page " + position;
+        }
     }
 }
